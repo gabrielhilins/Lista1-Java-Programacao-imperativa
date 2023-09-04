b7ff2aa2b88bd63174a2db7589ba128baf47104d
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2023-09-03 22:08:46 -0300

    Add files via upload

---
+++ lista1q8.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class lista1q8 {
+    public static void main(String[] args) {
+        int quantidade = 10;
+        int[] par = new int[quantidade];
+        int[] impar = new int[quantidade];
+        Random rand = new Random();
+
+        for (int i = 0; i < quantidade; i++) {
+            int numeroAleatorio = rand.nextInt(10) + 1; // Gera números entre 1 e 20
+
+            if (numeroAleatorio % 2 == 0) {
+                par[i] = numeroAleatorio;
+            } else {
+                impar[i] = numeroAleatorio;
+            }
+        }
+
+        System.out.println("Números Pares:");
+        exibirVetor(par);
+
+        System.out.println("\nNúmeros Ímpares:");
+        exibirVetor(impar);
+    }
+
+    public static void exibirVetor(int[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] != 0) {
+                System.out.print(vetor[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
